5a7e33bb337b881d4463e21fa25eb3983abd863c
Joao <vtrdll3@gmail.com>
2025-12-22 18:27:13 -0300

    error with git actions

--- app/core/settings.py
+++ app/core/settings.py
@@ -91,10 +91,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'postgres'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'db'),  # <- deve bater com o service do postgres
+        'NAME': os.environ.get(
+            'DB_NAME', 'postgres'
+        ),
+        'USER': os.environ.get(
+            'DB_USER', 'postgres'
+        ),
+        'PASSWORD': os.environ.get(
+            'DB_PASSWORD', 'postgres'
+         ),
+        'HOST': os.environ.get(
+            'DB_HOST', 'db'
+         ), 
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
